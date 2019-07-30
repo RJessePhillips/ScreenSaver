ec11634a1b9d9386d498857d48168583a029d4da
Jesse Phillips <51276233+RJessePhillips@users.noreply.github.com>
2019-07-30 15:18:47 -0400

    Add files via upload

---
+++ ScreenSaver/ScreenSaver/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace ScreenSaver
+{
+    partial class formScreenSaver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // formScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "formScreenSaver";
+            this.ShowInTaskbar = false;
+            this.Text = "ScreenSaver";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.formScreenSaver_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.formScreenSaver_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.formScreenSaver_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ScreenSaver/ScreenSaver/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class formScreenSaver : Form
+    {
+        List<Image> BGImages = new List<Image>();
+        List<BritPic> Britpics = new List<BritPic>();
+        Random rand = new Random();
+
+        //holds position of picture
+        class BritPic
+        {
+            public int PicNum;
+            public float X;
+            public float Y;
+            public float Speed;
+        }
+        public formScreenSaver()
+        {
+            InitializeComponent();
+        }
+
+        //exits program by touching any key
+        private void formScreenSaver_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+
+        private void formScreenSaver_Load(object sender, EventArgs e)
+        {
+            //get list of pics into images array
+            string[] images = System.IO.Directory.GetFiles("pics");
+            //loop through images
+            foreach (string image in images)
+            {
+                //creates new bitmap picture and add to BGImages array
+                BGImages.Add(new Bitmap(image));
+            }
+
+            //looks for images in directory and keep adding up to 50
+            for (int i = 0; i < 50; ++i)
+            {
+                BritPic np = new BritPic();
+                np.PicNum = i % BGImages.Count;
+                np.X = rand.Next(0, Width);
+                np.Y = rand.Next(0, Height);
+
+                Britpics.Add(np);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //enables screen refresh for interval length
+            Refresh();
+        }
+
+        private void formScreenSaver_Paint(object sender, PaintEventArgs e)
+        {
+            foreach(BritPic bp in Britpics)
+            {
+                //draws images on x,y axis
+                e.Graphics.DrawImage(BGImages[bp.PicNum], bp.X, bp.Y);
+                bp.X -= 2;
+
+                //if object moves off screen, move it to right of screen
+                if(bp.X < -250)
+                {
+                    bp.X = Width + rand.Next(20, 100);
+                }
+            }
+        }
+    }
+}
